b769225dd659202b17e4f3f43a4c9c194d6d917b
JlovesNoodles <91066411+JlovesNoodles@users.noreply.github.com>
2024-07-09 12:27:20 +0800

    Update subrecon.sh

--- subrecon.sh
+++ subrecon.sh
@@ -13,6 +13,15 @@ if [ ! -d "$url/subdomain" ];then
 	mkdir $url/subdomain
 fi
 
+if [ ! -d "$url/parameterfuzz" ];then
+	mkdir $url/parameterfuzz
+fi
+
+if [ ! -d "$url/gau" ];then
+	mkdir $url/gau
+fi
+
+
 
 function subdomainrecon(){
 	cowsay "Subdomain Recon Starting Now. Please Wait!!!"
@@ -66,39 +75,153 @@ paste
 
 
 
-function contentdiscovery(){
-	cowsay "Do you want to discover what informations are in these subdomains?"
-	echo "++++++++[Y]es or [N]o++++++++"
-	read content
-	if [[ $content == "Y" || $content == "y" ]]; then
-	/root/go/bin/httpx -status-code -title -tech-detect -list $url/subdomain/alivesubdomains.txt
-	else
-	echo " " 
-	echo " " 
-	echo " " 
-	echo "Aight understandable have a good day" | lolcat
-	echo " " 
-	echo " " 
-	echo " " 
-	fi
+function contentdiscovery() {
+	while true; do
 
+		cowsay "Do you want to DISCOVER TECH INFORMATIONS in these subdomains?" | lolcat
+		echo "++++++++[Y]es or [N]o++++++++"
+		read content
+		if [[ $content == "Y" || $content == "y" ]]; then
+			/root/go/bin/httpx -status-code -title -tech-detect -list $url/subdomain/alivesubdomains.txt
+		elif [[ $content == "N" || $content == "n" ]]; then
 
+			echo " "
+			echo " "
+			echo "Aight understandable have a good day" | lolcat
+			echo " "
+			echo " "
+			break
+		else
+			echo " "
+			echo "Bruh Really? Can't you choose Y and N?" | lolcat
+			continue
+		fi
+	done
 }
 contentdiscovery
 
-function subtakeover(){
-	cowsay "Do you want to test for subdomain takeovers?"
-	echo "++++++++[Y]es or [N]o++++++++"
-	read subtakoever
-	if [[ $subtakoever == "Y" || $subtakoever == "y" ]]; then
-	subjack -w $url/subdomain/alivesubdomains.txt -v 
-	else
-	cowsay -f dragon "Thank You Happy Hacking"
+
+function subtakeover() {
+    while true; do
+        cowsay "Do you want to test for SUBDOMAIN TAKEOVER?" | lolcat
+        echo "++++++++[Y]es or [N]o++++++++"
+        read subtakeover
+        if [[ $subtakeover == "Y" || $subtakeover == "y" ]]; then
+            subjack -w $url/subdomain/alivesubdomains.txt -v
+        elif [[ $subtakeover == "N" || $subtakeover == "n" ]]; then
+            echo " "
+            echo "Aight Understandable have a good day" | lolcat
+            break
+        else
+            echo " "
+            echo "Bruh Really? Can't you choose Y and N?" | lolcat
+            continue
+        fi
+    done
+}
+
+subtakeover
+
+
+
+
+function paramharvester() {
+    while true; do
+        cowsay "Do you want to Harvest the parameters for XSS Fuzzing for later?" | lolcat
+        echo "++++++++[Y]es or [N]o++++++++"
+        echo " "
+        read paramharvesterchoice
+        if [[ $paramharvesterchoice == "Y" || $paramharvesterchoice == "y" ]]; then
+            echo "Do you want all the subdomain or just the domain {S}ubdomain {D}omain" | lolcat
+            read subdomainchoice
+            if [[ $subdomainchoice == "D" || $subdomainchoice == "d" ]]; then
+                paramspider -d $url
+                mv results/$url.txt $url/parameterfuzz
+                rm -r results
+            elif [[ $subdomainchoice == "S" || $subdomainchoice == "s" ]]; then
+                paramspider -l $url/subdomain/alivesubdomains.txt
+                mv results/*.txt $url/parameterfuzz
+            else
+                echo " "
+                echo "Bruh you gotta be shitting me imma head out" | lolcat
+                exit
+            fi
+        elif [[ $paramharvesterchoice == "N" || $paramharvesterchoice == "n" ]]; then
+            echo " "
+            echo "Aight moving forward" | lolcat
+            break
+        else
+            echo "Bruh Really? Please enter 'Y' or 'N'."
+            echo " "
+            continue
+        fi
+
+		echo " "
+		echo " "
+		echo " "
+        echo "Do you want to scan again? [Y]es/[N]o" | lolcat
+        read scanagainchoice
+        if [[ $scanagainchoice == "Y" || $scanagainchoice == "y" ]]; then
+            continue
+        elif [[ $scanagainchoice == "N" || $scanagainchoice == "n" ]]; then
+            echo " "
+            echo "Aight, understandable have a good day" | lolcat
+            break
+        else
+            echo "Bruh Really? Please enter 'Y' or 'N'."
+            echo " "
+            continue
+        fi
+    done
+}
+
+paramharvester
+
+
+
+function gauresults(){
+	while true; do
+	echo " "
+	echo " "
+	echo " "
+	echo "[+] Getting URL's using GAU (.php .asa .inc .sql .zip .tar .pdf .txt Extenstions Only)" | lolcat
+	read gauchoice
+
+
+	 if [[ gauchoice == "Y" || gauchoice == "y" ]]; then
+
+            /root/bin/gau --fc 404,302 $url | grep ".php" >> $url/gau/$url.PHP_extentionurl.txt
+			/root/bin/gau --fc 404,302 $url | grep ".asa" >> $url/gau/$url.ASA_extentionurl.txt
+			/root/bin/gau --fc 404,302 $url | grep ".inc" >> $url/gau/$url.INC_extentionurl.txt
+			/root/bin/gau --fc 404,302 $url | grep ".sql" >> $url/gau/$url.SQL_extentionurl.txt
+			/root/bin/gau --fc 404,302 $url | grep ".zip" >> $url/gau/$url.ZIP_extentionurl.txt
+			/root/bin/gau --fc 404,302 $url | grep ".tar" >> $url/gau/$url.TAR_extentionurl.txt
+			/root/bin/gau --fc 404,302 $url | grep ".pdf" >> $url/gau/$url.PDF_extentionurl.txt
+			/root/bin/gau --fc 404,302 $url | grep ".txt" >> $url/gau/$url.TXT_extentionurl.txt
+
+        elif [[ gauchoice == "N" || gauchoice == "n" ]]; then
+            echo " "
+            echo "Aight, understandable have a good day" | lolcat
+            break
+        else
+            echo "Bruh Really? Please enter 'Y' or 'N'."
+            echo " "
+			continue
 	fi
+done
+
 
 
 
 }
-subtakeover
+gauresults
 
+
+
+
+
+echo " " 
+echo " " 
+echo " "
+cowsay -f dragon "Thank You Happy Hacking" | lolcat
 
